7fc586cd5238a172025643f60dcb6fb5b5689ea8
aa1-12 <70687449+aa1-12@users.noreply.github.com>
2020-09-17 17:53:15 +0900

    Add files via upload

---
+++ aa1-12-rpt02/start/aa12 info.js
@@ -0,0 +1,8 @@
+// aanninfo.js
+
+module.exports = function(id, name, phone) {
+    console.log("My info");
+    console.log("ID : " + id);
+    console.log("Name : " + name);
+    console.log("phone :" + phone + "\n");
+}
---
+++ aa1-12-rpt02/start/index_aa12.js
@@ -0,0 +1,7 @@
+// index _aann.js
+
+var myinfo = require("./aa12 info.js");
+
+myinfo("aa12", "Redwoods", '010-1234-5678');
+
+myinfo("aa55", "Comsi", "010-5678-1234");
